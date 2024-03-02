199cc413b78b8084a11bd9a06bac894b2b4b374e
KimJacobsen <157890151+JacobsenKim@users.noreply.github.com>
2024-03-02 19:32:56 +0100

    Update sql_import_remote.sh

--- ha2mariadb/sql_import_remote.sh
+++ ha2mariadb/sql_import_remote.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 # Domain: https://github.com/JacobsenKim/ha2mariadb
+# Wiki: https://github.com/JacobsenKim/ha2mariadb/wiki
+# Discussions: https://github.com/JacobsenKim/ha2mariadb/discussions
 #
 # sql_import_remote.sh
 #
